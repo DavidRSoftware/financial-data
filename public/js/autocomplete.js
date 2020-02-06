c676cddf3a506ff1f38c2c92fcd00c95585754a0
David Rosen <david.tzvi.rosen@gmail.com>
2020-02-06 15:51:04 -0500

    added error handling

--- public/js/autocomplete.js
+++ public/js/autocomplete.js
@@ -9,7 +9,8 @@ const createAutoComplete = ({
   root.innerHTML = `
   <div class="form-group">
   <label for="stockInput"><b>Search</b></label>
-  <input class="form-control" id="stockInput" type="text" placeholder="Start typing stock name or ticker">
+  <div class="ml-3 d-none inline-display text-danger" id="noResultsError">The ticker or stock name that has been entered has not returned results</div>
+  <input class="form-control" id="stockInput" type="text" placeholder="Start typing a stock name or ticker">
   </div>
   <div class="dropdown">
   <div class="dropdown-menu" id="results">
@@ -22,23 +23,26 @@ const createAutoComplete = ({
 
   const onInput = async event => {
     if (event.target.value !== "") {
+      document.querySelector("#noResultsError").classList.add("d-none");
       const items = await fetchData(event.target.value);
       if (items.length !== 0) {
         resultsWrapper.innerHTML = "";
-        $(resultsWrapper).toggle();
+        $(resultsWrapper).show();
         for (let item of items) {
           const option = document.createElement("a");
 
           option.classList.add("dropdown-item");
           option.innerHTML = renderOption(item);
           option.addEventListener("click", () => {
-            $(resultsWrapper).toggle();
+            $(resultsWrapper).hide();
             input.value = "";
             onOptionSelect(item);
           });
 
           resultsWrapper.appendChild(option);
         }
+      } else {
+        document.querySelector("#noResultsError").classList.remove("d-none");
       }
     }
   };
